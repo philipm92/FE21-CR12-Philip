bc1e4bd824bcff9c43e577d249ef132a456b92ac
philipm92 <philip.mahlberg@hotmail.com>
2021-08-27 23:30:38 +0200

    updated DB, change coordinates to text instead of float

--- database/fswd13_cr12_mount_everest_philip.sql
+++ database/fswd13_cr12_mount_everest_philip.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 27, 2021 at 03:13 PM
+-- Generation Time: Aug 27, 2021 at 11:29 PM
 -- Server version: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -35,8 +35,8 @@ CREATE TABLE `travel` (
   `location_name` varchar(100) NOT NULL,
   `price` decimal(13,2) NOT NULL,
   `description` text DEFAULT 'Lorem ipsum dolor sit amet.',
-  `longitude` float NOT NULL,
-  `latitude` float NOT NULL
+  `longitude` text DEFAULT NULL,
+  `latitude` text DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -44,10 +44,10 @@ CREATE TABLE `travel` (
 --
 
 INSERT INTO `travel` (`id`, `picture`, `location_name`, `price`, `description`, `longitude`, `latitude`) VALUES
-(1, 'ANiNite21_new.jpg', 'AniNite 2021', '25.00', 'Bei der größten, schönsten und besten Anime- und Manga-Convention Österreichs!*\r\nNach einer furchtbaren gefühlt endlosen Pause freut sich das gesamte AniNite-Team dich endlich wieder begrüßen zu dürfen!', 16.3126, 48.1134),
-(2, 'Area52_new.jpg', 'Area 52', '12.00', 'Die Area52 ist Österreichs größte Gaming Lounge und befindet sich im Herzen des 21. Wiener Gemeindebezirks. Nur 5 Gehminuten vom Bahnhof Floridsdorf entfernt kannst du einen ganzen Tag lang in die Welt der Videospiele eintauchen.', 16.4011, 48.2549),
-(3, 'stephansdom.JPG', 'St. Stephen\'s Cathedral', '0.00', 'St. Stephen\'s Cathedral (more commonly known by its German title: Stephansdom) is the mother church of the Roman Catholic Archdiocese of Vienna and the seat of the Archbishop of Vienna, Christoph Cardinal Schönborn, OP.', 16.3735, 48.2086),
-(4, 'travel_chicago_america_new.jpg', 'The Buckingham Hotel', '1432.00', 'Redefine your expectations of the hotel experience with The Buckingham Hotel. Our spacious rooms are tucked away on the 40th floor of the Chicago Stock Exchange Building and offer unparalleled views of the skyline and bustling cityscape. Watch the sunrise over the lake through our picturesque windows.', -87.6318, 41.8762);
+(1, '612920b4723ae.jpg', 'AniNite 2021', '20.00', 'Bei der größten, schönsten und besten Anime- und Manga-Convention Österreichs!*Nach einer furchtbaren gefühlt endlosen Pause freut sich das gesamte AniNite-Team dich endlich wieder begrüßen zu dürfen!', '16.312691169191364', '48.11331789876801'),
+(2, 'Area52_new.jpg', 'Area 52', '12.00', 'Die Area52 ist Österreichs größte Gaming Lounge und befindet sich im Herzen des 21. Wiener Gemeindebezirks. Nur 5 Gehminuten vom Bahnhof Floridsdorf entfernt kannst du einen ganzen Tag lang in die Welt der Videospiele eintauchen.', '16.400522540545637', '48.25373585169849'),
+(3, 'stephansdom.JPG', 'St. Stephen\'s Cathedral', '0.00', 'St. Stephen\'s Cathedral (more commonly known by its German title: Stephansdom) is the mother church of the Roman Catholic Archdiocese of Vienna and the seat of the Archbishop of Vienna, Christoph Cardinal Schönborn, OP.', '16.37350288472043', '48.20857582766164'),
+(4, 'travel_chicago_america_new.jpg', 'The Buckingham Hotel', '1432.00', 'Redefine your expectations of the hotel experience with The Buckingham Hotel. Our spacious rooms are tucked away on the 40th floor of the Chicago Stock Exchange Building and offer unparalleled views of the skyline and bustling cityscape. Watch the sunrise over the lake through our picturesque windows.', '-87.63186349455471', '41.87610771074468');
 
 --
 -- Indexes for dumped tables
@@ -67,7 +67,7 @@ ALTER TABLE `travel`
 -- AUTO_INCREMENT for table `travel`
 --
 ALTER TABLE `travel`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
